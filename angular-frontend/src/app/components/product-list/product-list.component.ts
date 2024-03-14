78936df8738ea45dcabbc6ddc6c7ab2dfcb893f3
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-14 15:39:35 +0700

    Add support for bootstrap and fontawesome

--- angular-frontend/src/app/components/product-list/product-list.component.ts
+++ angular-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  products: Product[] = []
+  constructor(private productService: ProductService){}
 
+  ngOnInit() : void{
+    this.listProducts();
+  }
+  listProducts() {
+    // method is invoked when subcribe is called
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
 }
--- angular-frontend/src/app/services/product.service.ts
+++ angular-frontend/src/app/services/product.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/product';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/api/products"
+
+  constructor(private httpClient: HttpClient) { }
+
+  // map json datas from spring to defined product array
+  getProductList(): Observable<Product[]>{
+    return this.httpClient.get<GetReponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products))
+  }
+}
+
+// unwrap json data to product
+interface GetReponse{
+  _embedded :{
+    products: Product[]
+  }
 }
