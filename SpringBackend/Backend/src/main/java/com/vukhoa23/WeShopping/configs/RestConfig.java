64494b7acc9402c2845d496a5b81b2bffdac24be
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-17 15:45:37 +0700

    add new component for dymamic showing dynamic categories

--- SpringBackend/Backend/src/main/java/com/vukhoa23/WeShopping/configs/RestConfig.java
+++ SpringBackend/Backend/src/main/java/com/vukhoa23/WeShopping/configs/RestConfig.java
@@ -2,13 +2,26 @@ package com.vukhoa23.WeShopping.configs;
 
 import com.vukhoa23.WeShopping.entities.Product;
 import com.vukhoa23.WeShopping.entities.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
+    private EntityManager entityManager;
+    @Autowired
+    public RestConfig(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
@@ -24,5 +37,23 @@ public class RestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        // expose id for entities
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        // get all classes manage by entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for(EntityType entityType : entities){
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        // expose id for entities
+        Class[] domainType =  entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainType);
     }
 }
--- angular-frontend/src/app/services/product.service.ts
+++ angular-frontend/src/app/services/product.service.ts
@@ -2,26 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../common/product';
 import { Observable, map } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  
+  private productsUrl = "http://localhost:8080/api/products"
 
-  private baseUrl = "http://localhost:8080/api/products"
+  private categoryUrl = "http://localhost:8080/api/product-category"
 
   constructor(private httpClient: HttpClient) { }
   // map json datas from spring to defined product array
   getProductList(currentCateId: number): Observable<Product[]>{
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${currentCateId}`
-    return this.httpClient.get<GetReponse>(searchUrl).pipe(
+    const searchUrl = `${this.productsUrl}/search/findByCategoryId?id=${currentCateId}`
+    return this.httpClient.get<GetReponseProduct>(searchUrl).pipe(
       map(response => response._embedded.products))
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetReponseCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory))
   }
 }
 
 // unwrap json data to product
-interface GetReponse{
+interface GetReponseProduct{
   _embedded :{
     products: Product[]
+  }
+}
+
+interface GetReponseCategory{
+  _embedded :{
+    productCategory: ProductCategory[]
   }
 }
