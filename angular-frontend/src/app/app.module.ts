0319a9f1abd54a43dd7793b9a5a46cf9bff084b5
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-03-20 01:53:32 +0700

    Created checkout form

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -13,8 +13,11 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
+  { path: "checkout", component: CheckoutComponent},
   { path: "cart-details", component: CartDetailsComponent},
   { path: "search/:keyword", component: ProductListComponent },
   { path: "category/:id", component: ProductListComponent },
@@ -34,6 +37,7 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
+    CheckoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,8 +45,8 @@ const routes: Routes = [
     // import http client module for fetching datas
     HttpClientModule,
     RouterModule.forRoot(routes),
-    NgbModule
-    
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration(),
